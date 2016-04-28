a9a63d861e8150ff01ee594646d2981ef1d458e7
xypista <csdivad@gmail.com>
2016-04-28 21:48:58 +0200

    PrimeFaces

---
+++ orai-munka-web/src/main/java/hu/sol/mik/book/faces/BookManagedBean.java
@@ -0,0 +1,75 @@
+package hu.sol.mik.book.faces;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import hu.sol.mik.book.bean.Book;
+import hu.sol.mik.book.dao.BookDao;
+import hu.sol.mik.book.dao.impl.BookDaoImpl;
+
+@ManagedBean
+@ViewScoped
+public class BookManagedBean {
+
+	private BookDao bookDao;
+	private List<Book> bookList;
+	private Book editedBook;
+	private List<Book> selectedBooks;
+
+	public BookManagedBean() {
+		this.bookDao = new BookDaoImpl();
+		listBooks();
+	}
+
+	private void listBooks() {
+		bookList = bookDao.listAll();
+	}
+
+	public List<Book> getBookList() {
+		return bookList;
+	}
+
+	public void newBook() {
+		editedBook = new Book();
+	}
+
+	public void saveBook() {
+		if (editedBook.getId() != null) {
+			bookDao.updateBook(editedBook);
+		} else {
+			bookDao.saveBook(editedBook);
+		}
+		listBooks();
+		editedBook = null;
+	}
+
+	public void deleteSelectedBooks() {
+		if (!selectedBooks.isEmpty()) {
+			for (Book b : selectedBooks) {
+				bookDao.delete(b);
+			}
+			listBooks();
+			System.out.println("ja");
+		}
+	}
+
+	public Book getEditedBook() {
+		return editedBook;
+	}
+
+	public void setEditedBook(Book editedBook) {
+		System.out.println(editedBook);
+		this.editedBook = editedBook;
+	}
+
+	public List<Book> getSelectedBooks() {
+		return selectedBooks;
+	}
+
+	public void setSelectedBooks(List<Book> selectedBooks) {
+		this.selectedBooks = selectedBooks;
+	}
+
+}
