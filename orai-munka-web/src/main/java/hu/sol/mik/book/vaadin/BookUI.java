130ad17b82dc8015145b0a0f8ef003b6ce00f03d
xypista <csdivad@gmail.com>
2016-05-06 17:23:30 +0200

    Make table cells editable by clicking on them

--- orai-munka-web/src/main/java/hu/sol/mik/book/vaadin/BookUI.java
+++ orai-munka-web/src/main/java/hu/sol/mik/book/vaadin/BookUI.java
@@ -3,21 +3,31 @@ package hu.sol.mik.book.vaadin;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.vaadin.data.Container;
+import com.vaadin.data.Property;
+import com.vaadin.data.Buffered.SourceException;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
 import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 import com.vaadin.data.util.BeanContainer;
 import com.vaadin.data.util.BeanItem;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.event.ShortcutListener;
+import com.vaadin.event.ItemClickEvent.ItemClickListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.Field;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.Table.ColumnGenerator;
+import com.vaadin.ui.TableFieldFactory;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -35,6 +45,8 @@ public class BookUI extends UI {
 	private BookDao bookDao;
 	protected Window editBookWindow;
 	protected List<Book> selectedBooks;
+	protected Object editItemId;
+	protected Object editPropertyId;
 
 	@Override
 	protected void init(VaadinRequest request) {
@@ -70,10 +82,9 @@ public class BookUI extends UI {
 			}
 		});
 
-		
 		Button deleteSelectedBooksButton = new Button("Kiválasztottak törlése");
 		deleteSelectedBooksButton.addClickListener(new ClickListener() {
-			
+
 			@Override
 			public void buttonClick(ClickEvent event) {
 				for (Book book : selectedBooks) {
@@ -81,9 +92,10 @@ public class BookUI extends UI {
 				}
 				selectedBooks.clear();
 				refreshTable();
+
 			}
 		});
-		
+
 		horizontalLayout.addComponent(newBookButton);
 		horizontalLayout.addComponent(deleteSelectedBooksButton);
 		return horizontalLayout;
@@ -109,14 +121,8 @@ public class BookUI extends UI {
 				try {
 					bookBinder.commit();
 					Book bean = bookBinder.getItemDataSource().getBean();
-					if (bean.getId() == null) {
-						bookDao.saveBook(bean);
-					} else {
-						bookDao.updateBook(bean);
-					}
+					saveOrUpdateBook(bean);
 					editBookWindow.close();
-					Notification.show("Sikeres felvitel");
-					refreshTable();
 				} catch (CommitException e) {
 					Notification.show("Hiba történt");
 				}
@@ -149,29 +155,11 @@ public class BookUI extends UI {
 	}
 
 	private Component createBookTable() {
-		Table table = new Table("Könyvek listája");
+		final Table table = new Table("Könyvek listája");
 		table.setSizeFull();
 		table.setContainerDataSource(bookDataSource);
 		table.setVisibleColumns("title", "description", "author", "pubYear");
-		table.addGeneratedColumn("editBook", new ColumnGenerator() {
-
-			@Override
-			public Object generateCell(final Table source, final Object itemId, final Object columnId) {
-				Button editButton = new Button("Módosít");
-				editButton.addClickListener(new ClickListener() {
-
-					@Override
-					public void buttonClick(ClickEvent event) {
-						BeanItem<Book> beanItem = (BeanItem<Book>) source.getItem(itemId);
-						openEditWindow(beanItem.getBean(), "Könyv szerkesztése");
-					}
-				});
-				return editButton;
-			}
-		});
-
 		table.addGeneratedColumn("selectBook", new ColumnGenerator() {
-
 			@Override
 			public Object generateCell(final Table source, final Object itemId, final Object columnId) {
 				final CheckBox selectCheckBox = new CheckBox();
@@ -190,14 +178,90 @@ public class BookUI extends UI {
 				return selectCheckBox;
 			}
 		});
-		
+
 		table.setColumnHeader("title", "Cím");
 		table.setColumnHeader("description", "Leírás");
 		table.setColumnHeader("author", "Szerző");
 		table.setColumnHeader("pubYear", "Publikálás éve");
-		table.setColumnHeader("editBook", "Szerkesztés");
 		table.setColumnHeader("selectBook", "Kiválasztás");
 
+		table.addItemClickListener(new ItemClickListener() {
+			@Override
+			public void itemClick(ItemClickEvent event) {
+				editItemId = event.getItemId();
+				editPropertyId = event.getPropertyId();
+				table.refreshRowCache();
+			}
+		});
+
+		table.setEditable(true);
+		table.setTableFieldFactory(new TableFieldFactory() {
+			@Override
+			public Field<?> createField(Container container, Object itemId, Object propertyId, Component uiContext) {
+				if (propertyId.equals(editPropertyId) && itemId.equals(editItemId)) {
+					return createEditTextField(table, itemId, propertyId);
+				}
+				return null;
+			}
+		});
+
 		return table;
+	}
+
+	private TextField createEditTextField(Table table, Object itemId, Object propertyId) {
+		Property editProperty = table.getContainerProperty(itemId, propertyId);
+		TextField textField = new TextField(editProperty);
+		textField.setNullRepresentation("");
+		textField.setBuffered(true);
+		textField.focus();
+		String editStartValue = textField.getValue();
+		if (editStartValue != null) {
+			textField.setCursorPosition(textField.getValue().length());
+		}
+
+		textField.addShortcutListener(
+				new ShortcutListener("EnterOnTextAreaShorcut", ShortcutAction.KeyCode.ESCAPE, null) {
+					@Override
+					public void handleAction(Object sender, Object target) {
+						editItemId = null;
+						editPropertyId = null;
+						table.refreshRowCache();
+					}
+				});
+
+		textField.addShortcutListener(
+				new ShortcutListener("EnterOnTextAreaShorcut", ShortcutAction.KeyCode.ENTER, null) {
+					@Override
+					public void handleAction(Object sender, Object target) {
+						String currentValue = textField.getValue();
+						if (currentValue == null ? editStartValue != null : !currentValue.equals(editStartValue)) {
+							try {
+								textField.commit();
+								BeanItem<Book> beanItem = (BeanItem<Book>) table.getItem(editItemId);
+								saveOrUpdateBook(beanItem.getBean());
+							} catch (InvalidValueException exception) {
+								Notification.show("Hibás bemenet");
+							} catch (SourceException exception) {
+								Notification.show("Hiba történt");
+
+							}
+						}
+						editItemId = null;
+						editPropertyId = null;
+						table.refreshRowCache();
+					}
+				});
+
+		return textField;
+	}
+
+	private void saveOrUpdateBook(Book book) {
+		if (book.getId() == null) {
+			bookDao.saveBook(book);
+			refreshTable();
+		} else {
+			bookDao.updateBook(book);
+		}
+		Notification.show("Sikeres felvitel");
 	}
 }
